ef8371efd1dc95eb66957b665e136195eb0d1b26
Efe <suleymanefeclk2@gmail.com>
2023-12-21 13:42:19 +0300

    Added some API implementation

--- app/api/auth/[...nextauth]/authOptions.ts
+++ app/api/auth/[...nextauth]/authOptions.ts
@@ -1,13 +1,45 @@
-import { NextAuthOptions } from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import DiscordProvider from "next-auth/providers/discord";
 
-const options: NextAuthOptions = {
-    providers: [
-        DiscordProvider({
-            clientId: process.env.DISCORD_CLIENT_ID!,
-            clientSecret: process.env.DISCORD_CLIENT_SECRET!,
-        }),
-    ],
-};
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
+}
 
-export default options;
+export const {
+  handlers: { GET, POST },
+  auth,
+} = NextAuth({
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    jwt({ token, profile, account }) {
+      if (profile && account) {
+        token.accessToken = account.access_token;
+        token.id = account?.userId;
+        token.image = profile.image;
+      }
+
+      return token;
+    },
+    session: ({ session, token }) => {
+      if (session?.user && token?.id) {
+        session.user.id = String(token.id);
+      }
+      return session;
+    },
+    // authorized({ auth }) {
+    //   return !!auth?.user; // this ensures there is a logged in user for -every- request
+    // },
+  },
+  pages: {
+    signIn: "/sign-in", // overrides the next-auth default signin page https://authjs.dev/guides/basics/pages
+  },
+});
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -4,8 +4,11 @@ import ChatInput from "./components/ChatInput";
 
 export default () => {
   return (
-    <div>
-      <ChatInput />
+    <div className="min-h-screen flex flex-col justify-between items-center">
+      <div></div>{" "}
+      <div className="w-full max-w-lg mb-3">
+        <ChatInput />
+      </div>
     </div>
   );
 };
