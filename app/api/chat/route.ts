ef39ce5a94c7dc78d12a952eccbc2d0098768908
Efe <suleymanefeclk2@gmail.com>
2023-12-31 19:02:59 +0300

    🔧 chore: switched to gpt4

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -40,7 +40,7 @@ export async function POST(req: NextRequest) {
   });
 
   const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
+    model: "gpt-4",
     messages,
     temperature: 0.7,
     stream: true,
