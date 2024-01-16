8c83270a63740bce353f26528719c589530050e9
Efe <suleymanefeclk2@gmail.com>
2024-01-15 16:51:15 -0800

    🔧 chore: changed the chat structure

--- app/chat/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import Chat from "../Chat";
-
-export default () => {
-    return <Chat />;
-};
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,11 +1,60 @@
 "use client";
 
-// import Chat from "@/components/chat/";
-// import { nanoid } from "@/lib/utils";
+import Chat from "./Chat";
+
+import { useCallback, useEffect, useState } from "react";
+
+import { createClient } from "@/utils/supabase/client";
+import { experimental_useAssistant as useAssistant } from "ai/react";
 
 export default () => {
-    // const id = nanoid();
+    const {
+        status,
+        messages,
+        input,
+        submitMessage,
+        handleInputChange,
+        error,
+        threadId,
+    } = useAssistant({
+        api: "/api/chat",
+    });
 
-    // return <Chat id={id} />;
-    return <div>Chat</div>;
+    const supabase = createClient();
+
+    const [loading, setLoading] = useState(true);
+    const [avatar_url, setAvatarUrl] = useState(null);
+
+    const getProfile = useCallback(async () => {
+        try {
+            setLoading(true);
+            const { data, error } = await supabase.auth.getUser();
+            if (error) {
+                throw error;
+            }
+
+            if (data) {
+                setAvatarUrl(data.user.user_metadata.avatar_url);
+            }
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    }, [supabase]);
+
+    useEffect(() => {
+        getProfile();
+    }, [getProfile]);
+
+    return (
+        <Chat
+            messages={messages}
+            avatar_url={avatar_url}
+            submitMessage={submitMessage}
+            handleInputChange={handleInputChange}
+            input={input}
+            status={status}
+        />
+    );
 };
