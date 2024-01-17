a728a37ac37618653ddd257606870513e43ca699
Ethan Henry <79533577+ethansocal@users.noreply.github.com>
2024-01-16 21:47:36 -0800

    Fix error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/SignUpUserSteps";
-import Header from "@/components/Header";
 import { cookies } from "next/headers";
 
 export default async function Index() {
